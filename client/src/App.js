f623805148854aa17d1c871e21789fef7cff1dd8
techdave <klatzdavie19@gmail.com>
2024-01-12 20:28:49 +0300

    create services component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Homepage } from './components/Homepage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Product from './components/product/Product';
 import About from './components/about/About';
+import Services from './components/services/Services';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path='/' element={<Homepage />} exact />
         <Route path='/shop' element={<Product />} exact />
         <Route path='/about' element={<About />} exact />
+        <Route path='/services' element={<Services />} exact />
       </Routes>
     </Router>
   );
---
+++ client/src/components/services/Services.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import Header from '../layout/Header';
+import Hero from '../layout/Hero';
+import Footer from '../layout/Footer';
+import { Link } from 'react-router-dom';
+
+const Services = () => {
+  return (
+    <>
+      <Header />
+      {/* Hero Section */}
+      <Hero>
+        <div className='row justify-content-between'>
+          <div className='col-lg-5'>
+            <div className='intro-excerpt'>
+              <h1>Services</h1>
+              <p className='mb-4'>
+                Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
+                velit. Aliquam vulputate velit imperdiet dolor tempor tristique.
+              </p>
+              <p>
+                <Link to='/shop' className='btn btn-secondary me-2'>
+                  Shop Now
+                </Link>
+                <Link href='/shop' className='btn btn-white-outline'>
+                  Explore
+                </Link>
+              </p>
+            </div>
+          </div>
+          <div className='col-lg-7'>
+            <div className='hero-img-wrap'>
+              <img src='images/couch.png' alt='' className='img-fluid' />
+            </div>
+          </div>
+        </div>
+      </Hero>
+
+      {/* Why Choose Us Section */}
+      <div className='why-choose-section'>
+        <div className='container'>
+          <div className='row my-5'>
+            <div className='col-6 col-md-6 col-lg-3 mb-4'>
+              <div className='feature'>
+                <div className='icon'>
+                  <img src='images/truck.svg' alt='' className='imf-fluid' />
+                </div>
+                <h3>Fast &amp; Free Shipping</h3>
+                <p>
+                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
+                  aliquet velit. Aliquam vulputate.
+                </p>
+              </div>
+            </div>
+
+            <div className='col-6 col-md-6 col-lg-3 mb-4'>
+              <div className='feature'>
+                <div className='icon'>
+                  <img src='images/bag.svg' alt='' className='imf-fluid' />
+                </div>
+                <h3>Easy to Shop</h3>
+                <p>
+                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
+                  aliquet velit. Aliquam vulputate.
+                </p>
+              </div>
+            </div>
+
+            <div className='col-6 col-md-6 col-lg-3 mb-4'>
+              <div className='feature'>
+                <div className='icon'>
+                  <img src='images/support.svg' alt='' className='imf-fluid' />
+                </div>
+                <h3>24/7 Support</h3>
+                <p>
+                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
+                  aliquet velit. Aliquam vulputate.
+                </p>
+              </div>
+            </div>
+
+            <div className='col-6 col-md-6 col-lg-3 mb-4'>
+              <div className='feature'>
+                <div className='icon'>
+                  <img src='images/return.svg' alt='' className='imf-fluid' />
+                </div>
+                <h3>Hassle Free Returns</h3>
+                <p>
+                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
+                  aliquet velit. Aliquam vulputate.
+                </p>
+              </div>
+            </div>
+
+            <div className='col-6 col-md-6 col-lg-3 mb-4'>
+              <div className='feature'>
+                <div className='icon'>
+                  <img src='images/truck.svg' alt='' className='imf-fluid' />
+                </div>
+                <h3>Fast &amp; Free Shipping</h3>
+                <p>
+                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
+                  aliquet velit. Aliquam vulputate.
+                </p>
+              </div>
+            </div>
+
+            <div className='col-6 col-md-6 col-lg-3 mb-4'>
+              <div className='feature'>
+                <div className='icon'>
+                  <img src='images/bag.svg' alt='' className='imf-fluid' />
+                </div>
+                <h3>Easy to Shop</h3>
+                <p>
+                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
+                  aliquet velit. Aliquam vulputate.
+                </p>
+              </div>
+            </div>
+
+            <div className='col-6 col-md-6 col-lg-3 mb-4'>
+              <div className='feature'>
+                <div className='icon'>
+                  <img src='images/support.svg' alt='' className='imf-fluid' />
+                </div>
+                <h3>24/7 Support</h3>
+                <p>
+                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
+                  aliquet velit. Aliquam vulputate.
+                </p>
+              </div>
+            </div>
+
+            <div className='col-6 col-md-6 col-lg-3 mb-4'>
+              <div className='feature'>
+                <div className='icon'>
+                  <img src='images/return.svg' alt='' className='imf-fluid' />
+                </div>
+                <h3>Hassle Free Returns</h3>
+                <p>
+                  Donec vitae odio quis nisl dapibus malesuada. Nullam ac
+                  aliquet velit. Aliquam vulputate.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+};
+
+export default Services;
