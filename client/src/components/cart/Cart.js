669ca021a69430e5c7657e8400ee5f2a04eaa5fd
techdave <klatzdavie19@gmail.com>
2024-01-13 07:21:21 +0300

    hot fixes

--- client/src/components/cart/Cart.js
+++ client/src/components/cart/Cart.js
@@ -49,7 +49,7 @@ const Cart = () => {
                       <td className='product-name'>
                         <h2 className='h5 text-black'>Product 1</h2>
                       </td>
-                      <td>$49.00</td>
+                      <td>Ksh. 5900</td>
                       <td>
                         <div
                           className='input-group mb-3 d-flex align-items-center quantity-container'
@@ -78,7 +78,7 @@ const Cart = () => {
                           </div>
                         </div>
                       </td>
-                      <td>$49.00</td>
+                      <td>Ksh. 5900</td>
                       <td>
                         <Link to='#' className='btn btn-black btn-sm'>
                           X
@@ -97,7 +97,7 @@ const Cart = () => {
                       <td className='product-name'>
                         <h2 className='h5 text-black'>Product 2</h2>
                       </td>
-                      <td>$49.00</td>
+                      <td>Ksh. 7800</td>
                       <td>
                         <div
                           className='input-group mb-3 d-flex align-items-center quantity-container'
@@ -126,7 +126,7 @@ const Cart = () => {
                           </div>
                         </div>
                       </td>
-                      <td>$49.00</td>
+                      <td>Ksh. 7800</td>
                       <td>
                         <Link to='#' className='btn btn-black btn-sm'>
                           X
@@ -140,39 +140,7 @@ const Cart = () => {
           </div>
 
           <div className='row'>
-            <div className='col-md-6'>
-              <div className='row mb-5'>
-                <div className='col-md-6 mb-3 mb-md-0'>
-                  <button className='btn btn-black btn-sm btn-block'>
-                    Update Cart
-                  </button>
-                </div>
-                <div className='col-md-6'>
-                  <button className='btn btn-outline-black btn-sm btn-block'>
-                    Continue Shopping
-                  </button>
-                </div>
-              </div>
-              <div className='row'>
-                <div className='col-md-12'>
-                  <label className='text-black h4' htmlFor='coupon'>
-                    Coupon
-                  </label>
-                  <p>Enter your coupon code if you have one.</p>
-                </div>
-                <div className='col-md-8 mb-3 mb-md-0'>
-                  <input
-                    type='text'
-                    className='form-control py-3'
-                    id='coupon'
-                    placeholder='Coupon Code'
-                  />
-                </div>
-                <div className='col-md-4'>
-                  <button className='btn btn-black'>Apply Coupon</button>
-                </div>
-              </div>
-            </div>
+            <div className='col-md-6'></div>
             <div className='col-md-6 pl-5'>
               <div className='row justify-content-end'>
                 <div className='col-md-7'>
@@ -188,7 +156,7 @@ const Cart = () => {
                       <span className='text-black'>Subtotal</span>
                     </div>
                     <div className='col-md-6 text-right'>
-                      <strong className='text-black'>$230.00</strong>
+                      <strong className='text-black'>Ksh. 13700</strong>
                     </div>
                   </div>
                   <div className='row mb-5'>
@@ -196,17 +164,19 @@ const Cart = () => {
                       <span className='text-black'>Total</span>
                     </div>
                     <div className='col-md-6 text-right'>
-                      <strong className='text-black'>$230.00</strong>
+                      <strong className='text-black'>Ksh. 13700</strong>
                     </div>
                   </div>
 
                   <div className='row'>
                     <div className='col-md-12'>
-                      <button
-                        className='btn btn-black btn-lg py-3 btn-block'
-                        onclick="window.location='checkout.html'">
-                        Proceed To Checkout
-                      </button>
+                      <Link to='/checkout'>
+                        <button
+                          className='btn btn-black btn-lg py-3 btn-block'
+                          onclick="window.location='checkout.html'">
+                          Proceed To Checkout
+                        </button>
+                      </Link>
                     </div>
                   </div>
                 </div>
